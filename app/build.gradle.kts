68441489a31b9b696cc42a3858396457131a3b35
Shyonei <116732291+akinemreyazici@users.noreply.github.com>
2024-04-16 00:13:02 +0300

    First successfully github library implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,8 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation(project(":networkmodule"))
+    //implementation(project(":networkmodule"))
+    implementation ("com.github.akinemreyazici:MultiVendorLibrary:1.0.7")
 
 
 }
